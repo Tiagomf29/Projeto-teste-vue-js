e83e014c9bc4bb39b53d0d5fc53d4d57169183fd
tiagomf29 <tiagomf29@hotmail.com>
2021-04-10 20:38:58 -0300

    projeto teste vue js

---
+++ src/components/Tiago.vue
@@ -0,0 +1,30 @@
+<template>
+<div id="tabela">
+    <h1 >Tiago Teste</h1>
+    <ul>
+        <li >Java Script</li>
+        <li>Java</li>
+        <li>Delphi</li>
+        <li>Python</li>
+    </ul>    
+</div>    
+</template>
+<script>
+    export default {  
+     
+    }
+
+</script>
+<style >
+   #tabela{
+       border: 1px solid;
+       position: relative;
+       left: 25%;
+       width: 50%;
+       background-color: yellow;
+       text-align: center;
+   }
+   li:hover{
+       background-color: teal;
+   }
+</style>
